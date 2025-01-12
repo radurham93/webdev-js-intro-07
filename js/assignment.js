e4f27ff88d822c2027626044f66bd8eb280982ae
Robert Durham <radurham93@gmail.com>
2025-01-12 17:01:33 -0500

    completed assignment

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,42 @@ const checkIfEvenElement = document.getElementById("check-if-even");
 const lostNumbersElement = document.getElementById("lost-numbers");
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
+
+let num1 = 5;
+let num2 = 4;
+let num3 = 47;
+
+function returnTheRemainder(num1, num2) {
+return num1 % num2;
+}
+
+
+function checkIfEven(num3){
+    /*if ( num3 % 2 === 0)
+       
+
+ { return true;}
+ else {return false;} 
+ */
+ return num3 %2 === 0;
+}
+
+
+function getTheFourthElement(lostNumbers){
+    return lostNumbers[3];
+}
+
+
+
+
+
+
+
+
+function render() {
+    remainderElement.textContent = returnTheRemainder(num1, num2);
+checkIfEvenElement.textContent = checkIfEven(num3);
+lostNumbersElement.textContent = getTheFourthElement(lostNumbers);
+}
+
+submissionBtn.addEventListener("click", function() {render();})
--- js/index.js
+++ js/index.js
@@ -1,4 +1,34 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
 
+function distance(lengthX, lengthY) {
+    return Math.sqrt((lengthX * lengthX) + (lengthY * lengthY));
+}
+
+let hypotenuse = distance(4, 5);
+console.log(hypotenuse);
+
+
+
+function add(a, b) {
+    return a + b;
+}
+
+let b = 4;
+let a = 69;
+
+console.log(add(a, b));
+
+
+
+function greeting (name = 'Dude!') {
+    console.log('Sup, ' + name);
+}
+
+greeting();
+greeting("Amigo");
+
+
+*/
